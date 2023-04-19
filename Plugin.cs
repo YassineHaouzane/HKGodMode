5608e9ffb2a8e6e6c467fc85815a1a4004291432
Yassine Haouzane <haouzaneyassine@gmail.com>
2023-04-19 18:27:13 +0200

    feat: adding nail arts damage

---
+++ Plugin.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace HKPlugin;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInProcess("hollow_knight.exe")]
+public class Plugin : BaseUnityPlugin
+{
+    internal static ManualLogSource Log;
+    public Harmony harmony { get; } = new(MyPluginInfo.PLUGIN_GUID);
+    private void Awake()
+    {
+        Log = base.Logger;
+        harmony.PatchAll();
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
+
+
+
+[HarmonyPatch(typeof(HeroController), nameof(HeroController.Pause))]
+class GodModePatch 
+{
+
+    static void Postfix()
+    {
+        var hero = HeroController.instance;
+        hero.playerData.isInvincible = !hero.playerData.isInvincible;
+        Plugin.Log.LogInfo($"Invincibility status: {hero.playerData.isInvincible}");
+        var tmpValue = hero.playerData.nailDamage;
+        // No idea why this is not used for basic nail attacks
+        hero.playerData.nailDamage = 500;
+        Plugin.Log.LogInfo($"Player nail arts damage increased from {tmpValue} to : {hero.playerData.nailDamage}");
+    }
+}
